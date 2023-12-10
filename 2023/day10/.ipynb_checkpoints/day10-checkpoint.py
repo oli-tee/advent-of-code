1421267c4ba884de33c5c447d3a82eb017220b4a
Oli Thuong <olit@spotify.com>
2023-12-10 22:46:27 +0100

    add day 10

--- 2023/day10/.ipynb_checkpoints/day10-checkpoint.py
+++ 2023/day10/.ipynb_checkpoints/day10-checkpoint.py
@@ -118,3 +118,110 @@ def show_path(data, distances,show_numbers=True):
                 matrix += ('.')
         matrix += ('\n')
     print(matrix)
+    
+    
+
+def clean_data(data):
+    graph = parse_graph(data)
+    start_pos = find_s(data)
+    distances = explore_graph(graph, start_pos)
+    matrix = []
+    n_rows = len(data)
+    n_cols = len(data[0])
+    for row in range(n_rows):
+        line = ""
+        for col in range(n_cols):                
+            if (row, col) in distances:
+                line += data[row][col]
+            else:
+                line += ('.')
+        matrix.append(line)
+        
+    # replace S
+    (s_row, s_col) = find_s(matrix)
+
+    up = ( s_row > 0 and matrix[s_row-1][s_col] in ('F', '|', '7'))
+    down = ( s_row < len(matrix)-1 and matrix[s_row+1][s_col] in ('L', '|', 'J'))
+    left = ( s_col > 0 and matrix[s_row][s_col-1] in ('F', '-', 'L'))
+    right = ( s_col < len(matrix[s_row])-1 and matrix[s_row][s_col+1] in ('J', '-', '7'))
+    if up and right:
+        actual_value = 'L'
+    elif up and left: 
+        actual_value = 'J'
+    elif down and right:
+        actual_value = 'F'
+    elif down and left: 
+        actual_value = '7'
+    elif down and up:
+        actual_value = '|'
+    elif left and right:
+        actual_value = '-'
+    try:
+        matrix[s_row] = matrix[s_row][:s_col] + actual_value + matrix[s_row][s_col+1:]
+    except:
+        print('ok')
+    
+    
+    return matrix
+
+
+def count_inside_dots_in_row(row: str, verbose=False):
+    is_in = False
+    counter = 0
+    for i in range(len(row)):
+        if row[i] in ['|']:
+            is_in = not is_in
+        elif row[i] in ['L', 'F']:
+            opener = row[i]
+        elif row[i] in ['7', 'J']:
+            closer = row[i]
+            if (opener == 'L' and closer == '7') or (opener == 'F' and closer == 'J'):
+                is_in = not is_in
+        elif row[i] == '.':
+            if is_in:
+                if verbose:
+                    print(i)
+                counter += 1
+    return counter
+
+def fill_row(row: str, verbose=False):
+    is_in = False
+    counter = 0
+    for i in range(len(row)):
+        if row[i] in ['|']:
+            is_in = not is_in
+        elif row[i] in ['L', 'F']:
+            opener = row[i]
+        elif row[i] in ['7', 'J']:
+            closer = row[i]
+            if (opener == 'L' and closer == '7') or (opener == 'F' and closer == 'J'):
+                is_in = not is_in
+        elif row[i] == '.':
+            if is_in:
+                row = row[:i] + 'O' + row[i+1:]
+                
+    #row = re.sub(r"[LF7J|-]", "x", row)
+    return row
+
+
+
+def solve_p2(file_name):
+    data = read_data(file_name)
+    cleaned_data = clean_data(data)
+    counts = [count_inside_dots_in_row(row) for row in cleaned_data]
+    rows_to_display = [prettify(fill_row(row)) for row in cleaned_data]
+    print("\n".join(rows_to_display))
+    return sum(counts)
+
+
+def prettify(s):
+    return (
+        s
+        .replace('J', '╝')
+        .replace('F', '╔')
+        .replace('L', '╚')
+        .replace('7', '╗')
+        .replace('|', '╢')
+        .replace('-', '╧')
+        
+    )
